cdc47b99c96fe6113cb509080a23ee710709f608
houmaolong <houmaolong@windows10.microdone.cn>
2019-04-25 21:57:06 +0800

    fsd

--- src/main/java/com/xiaopixiong/mall/domain/Goods.java
+++ src/main/java/com/xiaopixiong/mall/domain/Goods.java
@@ -12,8 +12,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 /**
  * 商品
  * 
